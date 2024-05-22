898e21e5c1d9ae291f1aa7ed12ca43bb859d0e68
Akriti-Kakkar <89967832+Akriti-Kakkar@users.noreply.github.com>
2024-05-23 00:20:30 +0530

    Add files via upload

---
+++ trial_Codes.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+# check multiple clicks
+password = st.text_input(placeholder='', label='passkey')
+if password == '1234':
+   df = pd.DataFrame({'test':['csv1']})
+   df1 = pd.DataFrame({'test':['csv1']})
+
+   # Define a function to convert the DataFrame to CSV format
+   @st.cache_data
+   def convert_df(df):
+      return df.to_csv(index=False).encode("utf-8")
+
+   # Convert the DataFrame to CSV
+   csv = convert_df(df)
+   csv1 = convert_df(df1)
+  # Create the download button
+   st.download_button("Press to Download", csv, "file.csv", "text/csv", key="download-csv")
+   st.download_button("Press to Download", csv1, "file.csv", "text/csv", key="download-csv-new")
+else:
+   st.info('Passkey is unavailable')
